fae98cfa49d92523d03673072b8d7c03489dfa96
shane <17680315365@163.com>
2024-03-08 14:42:39 +0530

    firt commit

---
+++ mall/mall-order/src/test/java/com/zsy/mallorder/mallorder/MallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsy.mallorder.mallorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
